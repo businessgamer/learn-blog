674891c2a3d820b0c129c354baa46f26f502affb
businessgamer <157469327+businessgamer@users.noreply.github.com>
2024-03-16 22:07:13 +0530

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -84,12 +84,13 @@
                 <div class="col-lg-4 col-md-12 col-sm-12">
                     <div class="subcribe-content">
                         <img src="{{ config('app.url') }}/assets/img/svg/big-mail.svg" alt="mail">
+                        <h3>Find Online Courses With Certificates</h3>
                     </div>
                 </div>
-                <div class="col-lg-8 col-md-12 col-sm-12">
+                <div class="col-lg-6 col-md-12 col-sm-12">
                     <div class="subscribe-from">
                         <form class="newsletter-form" data-toggle="validator">
-                            <input type="text" class="input-newsletter" placeholder="Search Your Favurit Skills Course"
+                            <input type="text" class="input-newsletter" placeholder="Search Your Favorite Skills Course"
                                    name="EMAIL" required autocomplete="off">
                             <button type="submit" class="default-btn">Get Started Now</button>
                             <div id="validator-newsletter3" class="form-result"></div>
