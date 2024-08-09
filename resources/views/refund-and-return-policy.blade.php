a5cf8711987e18b397e3fc82ea919a3958eea523
businessgamer <157469327+businessgamer@users.noreply.github.com>
2024-08-09 18:29:39 +0530

    Update refund-and-return-policy.blade.php

--- resources/views/refund-and-return-policy.blade.php
+++ resources/views/refund-and-return-policy.blade.php
@@ -1,1 +1,76 @@
-
+@extends('layouts.app')
+@section('content')
+    <!-- Start EduMim Page Title Area -->
+    <section class="page-title-area item-bg1">
+        <div class="container">
+            <div class="page-title-content">
+                <h1>Privacy Policy</h1>
+                <ul class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="#">Pages</a></li>
+                    <li class="breadcrumb-item"></li>
+                    <li class="primery-link">Policy</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <!-- End EduMim Page Title Area -->
+<h2>Introduction</h2>
+<p>Welcome to www.educated.co.in/  Managed by Jitendra Kumar ("we," "our," or "us"). We are dedicated to empowering dreams and inspiring excellence through education. Your privacy is important to us, and this Privacy Policy outlines how we collect, use, disclose, and safeguard your information when you visit our website, https://www.educated.co.in (the "Site"). By using our Site, you agree to the terms of this Privacy Policy.
+</p>
+<h3>Information We Collect</h3>
+<p>We may collect and process the following types of information:</p>
+<ul>
+    <li><b>Personal Information:</b> Information you provide directly, such as your name, email address, phone number, and other contact details when you register for an account, subscribe to our newsletter, or contact us.
+</li>
+    <li><b>Usage Data:</b> Information about your interactions with our Site, including your IP address, browser type, pages visited, time spent on pages, and other diagnostic data.
+</li>
+    <li><b>Cookies and Tracking Technologies:</b> We use cookies and similar technologies to track user activity and enhance your experience. Cookies are small data files placed on your device.
+</li>
+</ul>
+<h3>How We Use Your Information?</h3>
+<p>We use the information collected for the following purposes:</p>
+<ul>
+    <li><b>To Provide and Maintain Our Services:</b> To operate, maintain, and improve our Site and services.</li>
+    <li><b>To Communicate with You:</b> To send you updates, newsletters, and marketing materials, and respond to your inquiries (you may opt-out at any time).
+</li>
+    <li><b>To Personalize Your Experience:</b> To tailor the content and features of our Site to your preferences and interests.
+</li>
+    <li><b>To Analyze Usage:</b> To understand how users interact with our Site and to improve its functionality and user experience.
+</li>
+    <li><b>To Process Transactions:</b> To handle and process transactions you initiate on our Site.</li>
+    <li><b>To Ensure Security:</b> To detect, prevent, and address technical issues and to protect the security of our Site.
+</li>
+</ul>
+<h3>Sharing Your Information</h3>
+<p>We may share your information in the following circumstances:</p>
+<ul>
+<li><b>With Service Providers:</b> We may share your information with third-party service providers who assist us in operating our Site and providing our services (e.g., email delivery, data analytics).
+</li>
+    <li><b>For Legal Compliance:</b> We may disclose your information if required to do so by law or in response to valid requests by public authorities (e.g., to comply with a subpoena).
+</li>
+    <li><b>Business Transfers:</b> In the event of a merger, acquisition, or asset sale, your information may be transferred as part of the transaction.
+</li>
+</ul>
+<h3>Security of Your Information</h3>
+    <p>We implement reasonable security measures to protect your personal information against unauthorized access, alteration, disclosure, or destruction. However, no method of transmission over the Internet or electronic storage is completely secure, and we cannot guarantee absolute security.
+</p>
+<h3>Your Rights and Choices</h3>
+<ul>
+    <li><b>Access and Update:</b> You can access and update your personal information by logging into your account or contacting us directly.
+</li>
+    <li><b>Opt-Out:</b> You can opt out of receiving promotional communications by following the unsubscribe instructions in those communications.
+</li>
+    <li><b>Cookies:</b> You can manage your cookie preferences through your browser settings. Please note that disabling cookies may affect the functionality of our Site.
+</li>
+</ul>
+<h3>Children’s Privacy</h3>
+<p>Our Site is not intended for children under the age of 13. We do not knowingly collect personal information from children under 13. If we become aware that we have collected personal information from a child under 13, we will take steps to delete such information.
+</p>
+<h3>8. Changes to This Privacy Policy</h3>
+<p>We may update this Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on our Site and updating the "Effective Date" at the top. Your continued use of the Site after any changes constitutes your acceptance of the new Privacy Policy.
+</p>
+<h3>Contact Us</h3>
+<p>If you have any questions or concerns about this Privacy Policy or our data practices, please contact us at:
+<br>Email: [your-email@example.com]
+</p>
+  @endsection
