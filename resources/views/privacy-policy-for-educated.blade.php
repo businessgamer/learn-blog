227461671e1f26651942a081fd99cbf448bc5049
businessgamer <157469327+businessgamer@users.noreply.github.com>
2024-08-09 13:14:45 +0530

    Update privacy-policy-for-educated.blade.php

--- resources/views/privacy-policy-for-educated.blade.php
+++ resources/views/privacy-policy-for-educated.blade.php
@@ -4,7 +4,7 @@
     <section class="page-title-area item-bg1">
         <div class="container">
             <div class="page-title-content">
-                <h2>Courses</h2>
+                <h1>Privacy Policy</h1>
                 <ul class="breadcrumb">
                     <li class="breadcrumb-item"><a href="#">Pages</a></li>
                     <li class="breadcrumb-item"></li>
@@ -14,5 +14,32 @@
         </div>
     </section>
     <!-- End EduMim Page Title Area -->
-
+<h2>Introduction</h2>
+<p>Welcome to www.educated.co.in/  Managed by Jitendra Kumar ("we," "our," or "us"). We are dedicated to empowering dreams and inspiring excellence through education. Your privacy is important to us, and this Privacy Policy outlines how we collect, use, disclose, and safeguard your information when you visit our website, https://www.educated.co.in (the "Site"). By using our Site, you agree to the terms of this Privacy Policy.
+</p>
+<h3>Information We Collect</h3>
+<p>We may collect and process the following types of information:</p>
+<ul>
+    <li>Personal Information: Information you provide directly, such as your name, email address, phone number, and other contact details when you register for an account, subscribe to our newsletter, or contact us.
+</li>
+    <li>Usage Data: Information about your interactions with our Site, including your IP address, browser type, pages visited, time spent on pages, and other diagnostic data.
+</li>
+    <li>Cookies and Tracking Technologies: We use cookies and similar technologies to track user activity and enhance your experience. Cookies are small data files placed on your device.
+</li>
+</ul>
+<h3>How We Use Your Information?</h3>
+<p>We use the information collected for the following purposes:</p>
+<ul>
+    <li>To Provide and Maintain Our Services: To operate, maintain, and improve our Site and services.</li>
+    <li>To Communicate with You: To send you updates, newsletters, and marketing materials, and respond to your inquiries (you may opt-out at any time).
+</li>
+    <li>To Personalize Your Experience: To tailor the content and features of our Site to your preferences and interests.
+</li>
+    <li>To Analyze Usage: To understand how users interact with our Site and to improve its functionality and user experience.
+</li>
+    <li>To Process Transactions: To handle and process transactions you initiate on our Site.</li>
+    <li>To Ensure Security: To detect, prevent, and address technical issues and to protect the security of our Site.
+</li>
+</ul>
+<h3>4. Sharing Your Information</h3>
   @endsection
